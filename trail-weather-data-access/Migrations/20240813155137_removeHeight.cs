db1b05841443370c37b0c0251448b3b348459ab7
MarekHaydenItera <marek.hayden@itera.com>
2024-08-13 17:55:36 +0200

    show migrations to Igor

---
+++ trail-weather-data-access/Migrations/20240813155137_removeHeight.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trail_weather_data_access.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeHeight : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "GeoData");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Height",
+                table: "GeoData",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
